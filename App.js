a3c861a0d733198e4c350b837035935c56526584
Tushar-92 <ts.aerotab@gmail.com>
2023-08-21 08:03:25 +0530

    Episode-3

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
-const heading = React.createElement("h1", {id:"heading"}, "Hello World From React!!");
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const heading = <h1 id="heading" className="heading">This is React From JSX</h1>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(heading);
+
+
+
+
+
 
